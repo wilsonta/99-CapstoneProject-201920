210b07b8284205a4d672e9e2d508935faf5e966d
wenningr <wenningr@rose-hulman.edu>
2019-02-06 09:45:00 -0500

    Added test code to m3

--- src/m3_run_this_on_robot.py
+++ src/m3_run_this_on_robot.py
@@ -23,7 +23,7 @@ def main():
 
 def run_test_arm():
     robot = rosebot.RoseBot()
-    robot.arm_and_claw.raise_arm()
+    #robot.arm_and_claw.raise_arm()
     robot.arm_and_claw.calibrate_arm()
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
