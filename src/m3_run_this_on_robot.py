79903b27f9671fe309c5367474a1106815c744d5
wenningr <wenningr@rose-hulman.edu>
2019-02-06 09:15:13 -0500

    Added test code to m3

--- src/m3_run_this_on_robot.py
+++ src/m3_run_this_on_robot.py
@@ -17,7 +17,13 @@ def main():
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
 
+    run_test_arm()
 
+
+
+def run_test_arm():
+    robot = rosebot.RoseBot()
+    robot.arm_and_claw.raise_arm()
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
