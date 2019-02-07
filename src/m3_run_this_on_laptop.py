2c236a7272eaabb751db1a73c422c8fb20a50a5a
wenningr <wenningr@rose-hulman.edu>
2019-02-07 08:51:26 -0500

    updated m3

--- src/m3_run_this_on_laptop.py
+++ src/m3_run_this_on_laptop.py
@@ -35,12 +35,13 @@ def main():
     # -------------------------------------------------------------------------
     # The main frame, upon which the other frames are placed.
     # -------------------------------------------------------------------------
-
+    main_frame = ttk.Frame(root, padding=10, borderwidth=5, relief='groove')
+    main_frame.grid()
 
     # -------------------------------------------------------------------------
     # Sub-frames for the shared GUI that the team developed:
     # -------------------------------------------------------------------------
-
+    teleop_frame, arm_frame, control_frame = get_shared_frames(main_frame, mqtt_sender)
 
     # -------------------------------------------------------------------------
     # Frames that are particular to my individual contributions to the project.
@@ -50,7 +51,7 @@ def main():
     # -------------------------------------------------------------------------
     # Grid the frames.
     # -------------------------------------------------------------------------
-
+    grid_frames(teleop_frame, arm_frame, control_frame)
 
     # -------------------------------------------------------------------------
     # The event loop:
@@ -59,11 +60,17 @@ def main():
 
 
 def get_shared_frames(main_frame, mqtt_sender):
-    pass
+    teleop_frame = shared_gui.get_teleoperation_frame(main_frame, mqtt_sender)
+    arm_frame= shared_gui.get_arm_frame(main_frame, mqtt_sender)
+    control_frame = shared_gui.get_control_frame(main_frame, mqtt_sender)
+
+    return teleop_frame, arm_frame, control_frame
 
 
 def grid_frames(teleop_frame, arm_frame, control_frame):
-    pass
+    teleop_frame.grid(row=0, column=0)
+    arm_frame.grid(row=1, column=0)
+    control_frame.grid(row=2, column=0)
 
 
 # -----------------------------------------------------------------------------
--- src/m3_run_this_on_robot.py
+++ src/m3_run_this_on_robot.py
@@ -23,8 +23,8 @@ def main():
 
 def run_test_arm():
     robot = rosebot.RoseBot()
-    #robot.arm_and_claw.raise_arm()
     robot.arm_and_claw.calibrate_arm()
+    robot.arm_and_claw.lower_arm()
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
