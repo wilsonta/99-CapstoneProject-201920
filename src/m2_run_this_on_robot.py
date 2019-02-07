1ee600a19b71827e0c916cac3fb2afa932a3a7af
whitemg1 <whitemg1@rose-hulman.edu>
2019-02-07 09:46:06 -0500

    Dr. M helped me lol

--- src/m2_run_this_on_robot.py
+++ src/m2_run_this_on_robot.py
@@ -17,6 +17,7 @@ def main():
       1. Makes the EV3 robot to various things.
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
+    real_thing()
 def real_thing():
     robot = rosebot.RoseBot()
     delegate = shared_gui_delegate_on_robot.Respond_to_GUI_message(robot)
