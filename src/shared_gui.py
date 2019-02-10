cffdc165c664c1f5a743e74c43d4c91c29c32cbb
whitemg1 <whitemg1@rose-hulman.edu>
2019-02-10 12:22:59 -0500

    Dr. M helped me lol

--- src/shared_gui.py
+++ src/shared_gui.py
@@ -189,7 +189,10 @@ def handle_left(left_entry_box, right_entry_box, mqtt_sender):
       :type  right_entry_box:  ttk.Entry
       :type  mqtt_sender:      com.MqttClient
     """
-
+    print('left',left_entry_box.get(),right_entry_box.get())
+    left = -int(left_entry_box.get())
+    right = int(right_entry_box.get())
+    mqtt_sender.send_message('go',[str(left),str(right)])
 
 def handle_right(left_entry_box, right_entry_box, mqtt_sender):
     """
@@ -199,6 +202,10 @@ def handle_right(left_entry_box, right_entry_box, mqtt_sender):
       :type  right_entry_box:  ttk.Entry
       :type  mqtt_sender:      com.MqttClient
     """
+    print('right',left_entry_box.get(),right_entry_box.get())
+    left = int(left_entry_box.get())
+    right = -int(right_entry_box.get())
+    mqtt_sender.send_message('go',[str(left),str(right)])
 
 
 def handle_stop(mqtt_sender):
@@ -206,6 +213,10 @@ def handle_stop(mqtt_sender):
     Tells the robot to stop.
       :type  mqtt_sender:  com.MqttClient
     """
+    print('stop')
+    left = 0
+    right = 0
+    mqtt_sender.send_message('go',[left,right])
 
 
 ###############################################################################
