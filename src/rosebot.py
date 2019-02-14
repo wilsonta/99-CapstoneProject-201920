601f83c45ccbefc105d4d916012046b5afe3a03e
whitemg1 <whitemg1@rose-hulman.edu>
2019-02-13 19:41:33 -0500

    Sorry Tim

--- src/rosebot.py
+++ src/rosebot.py
@@ -303,7 +303,7 @@ class DriveSystem(object):
                 tone_maker.play_tone(orig_tone_freq, 250).wait()
                 orig_tone_freq = orig_tone_freq + delta_tone
                 orig_distance = self.sensor_system.ir_proximity_sensor.get_distance_in_inches()
-                if self.sensor_system.ir_proximity_sensor.get_distance_in_inches() <= 6:
+                if self.sensor_system.ir_proximity_sensor.get_distance_in_inches() <= 2:
                     self.stop()
                     break
 
